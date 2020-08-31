f3a426a9c1b6c0e2819fa52b98054a88c9d00d75
Sriharsha Chellaboina <harshachel@gmail.com>
2020-08-31 18:03:40 +0530

    Defined functions in text.h

--- Assignment1/text.h
+++ Assignment1/text.h
@@ -1,4 +1,22 @@
 #ifndef TEXT_H
 #define TEXT_H
-
+void put20nos(FILE file)
+{
+	int i;
+	for (i=0;i++;i<20)
+	{
+		fgetw(i,file);
+	}
+	return 0;
+}
+void read20nos(FILE file)
+{
+	int i;
+	while(!feof(file))
+	{
+		fscanf(file, "%d", &i);
+		printf("%i/n",i);
+	}
+	return 0;
+}
 #endif
